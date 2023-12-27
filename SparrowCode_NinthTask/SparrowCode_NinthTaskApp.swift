8160bfcb7b9826314ee7502b809b3ff5965aa12d
Edmond <edpodlegaev@yandex.ru>
2023-12-27 14:03:22 +0300

    Initial Commit

---
+++ SparrowCode_NinthTask/SparrowCode_NinthTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparrowCode_NinthTaskApp.swift
+//  SparrowCode_NinthTask
+//
+//  Created by Edmond Podlegaev on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparrowCode_NinthTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
